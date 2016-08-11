48af6c602d66f18124977cf990fa901dfd438c1e
Jon Abrams <jon@jonabrams.com>
2016-08-11 16:04:20 -0700

    Add remove listener

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+function removeTodo () {
+  alert("REMOVE!");
+}
+
 $(function () {
+  $('.todo-remove').on('click', removeTodo);
+
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
     newTodo.find('.todo-text').val('');
+    newTodo.find('.todo-checkbox').prop('checked', false);
+    newTodo.find('.todo-remove').on('click', removeTodo);
     newTodo.appendTo('.todo-list');
   });
 });
