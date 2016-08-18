07b1719656f90d14c50f000a36442278c229465c
Jon Abrams <jon@jonabrams.com>
2016-08-18 16:14:09 -0700

    Move todos between sections

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 function removeTodo (event) {
+  debugger;
   var target = event.currentTarget;
   $(target).parent().remove();
 }
 
-$(function () {
+function moveToCompleted (event) {
+  var checkbox = $(event.currentTarget);
+  var todo = checkbox.parent();
+  todo.remove();
+  todo.prependTo('#completed-list');
+  todo.find('.todo-remove').on('click', removeTodo);
+  checkbox.on('click', moveToCurrent);
+}
+
+function moveToCurrent (event) {
+  var checkbox = $(event.currentTarget);
+  var todo = checkbox.parent();
+  todo.remove();
+  todo.prependTo('#current-list');
+  todo.find('.todo-remove').on('click', removeTodo);
+  checkbox.on('click', moveToCompleted);
+}
+
+$(document).ready(function () {
   $('.add-todo').on('click', function () {
     var newTodo = $('.todo').first().clone();
     newTodo.removeClass('hide');
     newTodo.find('.todo-remove').on('click', removeTodo);
-    newTodo.appendTo('.todo-list');
+    newTodo.find('.todo-checkbox').on('click', moveToCompleted);
+    newTodo.appendTo('#current-list');
   });
 });
